05fe8535186099100d1a3d654e4a6d2ad9235cb9
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-19 11:04:19 +0200

    Add string message to 403 Excpetion

--- ITBees.RestfulApiControllers/Authorization/Authorization403ForbiddenException.cs
+++ ITBees.RestfulApiControllers/Authorization/Authorization403ForbiddenException.cs
@@ -4,6 +4,11 @@ namespace ITBees.RestfulApiControllers.Authorization;
 
 public class Authorization403ForbiddenException : Exception
 {
+    public Authorization403ForbiddenException(string message) : base (message)
+    {
+        
+    }
+
     public Authorization403ForbiddenException(AuthorizationExceptionMessages message) : base(message.ToString())
     {
 
