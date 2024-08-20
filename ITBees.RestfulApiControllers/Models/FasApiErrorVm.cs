9299e71a266e1d68d60c5eb5815b255cf10c5274
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-20 09:05:39 +0200

    Common api error view models

---
+++ ITBees.RestfulApiControllers/Models/FasApiErrorVm.cs
@@ -0,0 +1,16 @@
+﻿namespace ITBees.RestfulApiControllers.Models;
+
+public class FasApiErrorVm
+{
+    public int StatusCode { get; }
+    public string ErrorKey { get; }
+
+    public FasApiErrorVm(string message, int statusCode, string errorKey)
+    {
+        StatusCode = statusCode;
+        Message = message ?? string.Empty;
+        ErrorKey = errorKey ?? string.Empty;
+    }
+
+    public string Message { get; set; }
+}
