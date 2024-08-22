6faef4cac8908b14c7ee18d0ca26b4ec6fcf7bee
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-22 15:52:41 +0200

    update nugets

--- ITBees.RestfulApiControllers/Models/FasApiErrorVm.cs
+++ ITBees.RestfulApiControllers/Models/FasApiErrorVm.cs
@@ -1,10 +1,18 @@
-﻿namespace ITBees.RestfulApiControllers.Models;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ITBees.RestfulApiControllers.Models;
 
 public class FasApiErrorVm
 {
     public int StatusCode { get; }
     public string ErrorKey { get; }
 
+    /// <summary>
+    /// Create instance of FasApiErrorVm, which will be used to generate error respone, based on provided status code
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="statusCode">Please provide correct status code for example use static class code like : StatusCodes.Status403Forbidden</param>
+    /// <param name="errorKey"></param>
     public FasApiErrorVm(string message, int statusCode, string errorKey)
     {
         StatusCode = statusCode;
