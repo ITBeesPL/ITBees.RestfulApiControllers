9a5cc55fb39dccd32b5e62d9f2d4c23c0c4a9c45
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-22 15:05:57 +0200

    FasApiError handling

---
+++ ITBees.RestfulApiControllers/Exceptions/FasApiErrorException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ITBees.RestfulApiControllers.Models;
+
+namespace ITBees.RestfulApiControllers.Exceptions;
+
+public class FasApiErrorException : Exception
+{
+    public FasApiErrorException(string message, FasApiErrorVm fasApiErrorVm) : base(message)
+    {
+        FasApiErrorVm = fasApiErrorVm;
+    }
+
+    public FasApiErrorVm FasApiErrorVm { get; }
+}
